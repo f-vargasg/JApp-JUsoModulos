6c7947dbc68d3cd50843c3a2d611f0d8e46d49bc
Francisco Vargas G. <52474105+f-vargasg@users.noreply.github.com>
2023-07-20 14:33:48 -0600

    Ejemplo dependencias.  Este proyecto se usa en combianción con JavaModuleTest2

---
+++ src/main/java/com/fvgprinc/app/usomodulos/UsoProductos.java
@@ -0,0 +1,27 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.fvgprinc.app.usomodulos;
+
+import com.fvgprinc.app.javamoduletest2.producto.Producto;
+
+/**
+ *
+ * @author garfi
+ */
+public class UsoProductos {
+
+    public static void main(String[] args) {
+        Producto miProducto = new Producto();
+        
+        miProducto.setPrecio(15000);
+        
+        String scrap = "Esto es una prueba";
+        
+       
+        
+        System.out.println( miProducto.getDescuento());
+    }
+}
